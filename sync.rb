bed63a467361aaaab87802d4be2f5c1d9dad31cc
Matthew Landauer <matthew@openaustralia.org>
2010-11-29 06:54:00 +1100

    Now has the potential for multi-way synching

--- sync.rb
+++ sync.rb
@@ -7,29 +7,50 @@ require 'yaml'
 require 'nokogiri'
 require 'rest_client'
 
-class Issue
-  attr_accessor :title, :github_id, :pivotal_id
-  def initialize(title, github_id, pivotal_id)
-    @title, @github_id, @pivotal_id = title, github_id, pivotal_id
+class SynchedIssue
+  attr_accessor :title, :ids
+  def initialize(title, ids)
+    @title, @ids = title, ids
   end
 end
 
-class Github
+class Issue
+  attr_accessor :title, :repo_id
+  def initialize(title, repo_id)
+    @title, @repo_id = title, repo_id
+  end
+end
+
+
+class Repo
+  attr_accessor :issues_to_be_synched
+
+  def mark_as_synched(id)
+    @issues_to_be_synched.delete_if {|i| i.repo_id == id}
+  end
+
+  def find_issue_to_be_synched(id)
+    @issues_to_be_synched.find {|i| i.repo_id == id}
+  end
+end
+
+class Github < Repo
+  attr_reader :open_issues
+
   def initialize(config)
     @username = config["username"]
     @api_token = config["api_token"]
     @repository = config["repository"]
-  end
-  
-  def open_issues
+
     github = open("https://github.com/api/v2/json/issues/list/#{@repository}/open",
       :http_basic_authentication=>["#{@username}/token", @api_token]) do |f|
       JSON.parse(f.read)
     end
 
-    github["issues"].map do |issue|
-      Issue.new(issue["title"], issue["number"], nil)
+    @open_issues = github["issues"].map do |issue|
+      Issue.new(issue["title"], issue["number"])
     end
+    @issues_to_be_synched = @open_issues
   end
   
   # Returns number of new issue
@@ -52,9 +73,15 @@ class Github
   def issues_name
     "GitHub issues"
   end
+  
+  def tag
+    :github
+  end
 end
 
-class Pivotal
+class Pivotal < Repo
+  attr_reader :open_issues, :issues_to_be_synched
+  
   def initialize(config)
     @username = config["username"]
     @password = config["password"]
@@ -63,12 +90,10 @@ class Pivotal
     @token = Nokogiri::XML(open("https://www.pivotaltracker.com/services/v3/tokens/active",
       :http_basic_authentication => [@username, @password])).at('guid').inner_text
     @project_id = api_v3("projects").search('project').find {|p| p.at('name').inner_text == @project}.at('id').inner_text
-  end
-  
-  def open_issues
-    @open_issues ||= api_v3("projects/#{@project_id}/stories").search('story').map do |s|
-      Issue.new(s.at('name').inner_text, nil, s.at('id').inner_text)
+    @open_issues = api_v3("projects/#{@project_id}/stories").search('story').map do |s|
+      Issue.new(s.at('name').inner_text, s.at('id').inner_text)
     end
+    @issues_to_be_synched = @open_issues
   end
   
   def api_v3(call)
@@ -95,6 +120,10 @@ class Pivotal
   def issues_name
     "Pivotal Tracker Stories"
   end
+  
+  def tag
+    :pivotal
+  end
 end
 
 # Given an array of values return the first found value that appears multiple times
@@ -106,28 +135,29 @@ def first_duplicate(a)
   nil
 end
 
+# Returns an array of all the duplicate values in an array
+# e.g. [1, 2, 3, 1, 4, 5, 6, 3, 7] => [1, 3]
+def all_duplicates(a)
+  a.uniq.select{|v| a.select{|b| b == v}.count > 1}
+end
+
 config = open("configuration.yaml") do |f|
   YAML.load(f.read)
 end
 
+puts "Retrieving issues..."
 g = Github.new(config["github"])
 p = Pivotal.new(config["pivotal"])
 s = [g, p]
 
+puts "Checking for duplicate titles..."
 s.each do |r|
-  puts "Getting #{r.issues_name}..."
-  r.open_issues
-
-  puts "Checking for duplicate titles..."
   duplicate_title = first_duplicate(r.open_issues.map{|i| i.title})
   if duplicate_title
     puts "Warning: There are multiple #{r.issues_name} with the same title: #{duplicate_title}. Please merge them manually into one and rerun the sync."
     exit
   end 
 end
-
-github_issues = g.open_issues
-pivotal_issues = p.open_issues
 
 synched = []
 
@@ -138,57 +168,80 @@ if File.exist?("issue-sync-store.yaml")
   end
   # For each issue in the store see if any of them have changed title
   store.each do |store_issue|
-    github_issue = github_issues.find {|i| i.github_id == store_issue.github_id}
-    pivotal_issue = pivotal_issues.find {|i| i.pivotal_id == store_issue.pivotal_id}
-    if github_issue.nil? || pivotal_issue.nil?
+    issues = s.map {|t| t.find_issue_to_be_synched(store_issue.ids[t.tag])}
+    
+    # If some of the issues couldn't be found - i.e. have been deleted in the meantime
+    if issues.compact != issues
       # The issue has been deleted. Keep the issue in the sync store to ensure that it doesn't get recreated
       synched << store_issue
     else
-      github_changed = (store_issue.title != github_issue.title)
-      pivotal_changed = (store_issue.title != pivotal_issue.title)
-      if github_changed && pivotal_changed
-        puts "Warning: The issue with the title '#{store_issue.title} was changed to '#{github_issue.title}' on #{g.system_name} and '#{pivotal_issue}' on #{p.system_name}. As both of them were changed we can't sync the changes"
+      issues_changed = issues.map{|i| store_issue.title != i.title}
+
+      if issues_changed.select{|b| b}.count > 1
+        puts "Warning: The issue with the title '#{store_issue.title}' was changed in more than one place to different values:"
+        s.each do |t|
+          issue = t.find_issue_to_be_synched(store_issue.ids[t.tag])
+          puts "  On #{t.system_name} it was changed to '#{issue.title}'" if issue.title != store_issue.title
+        end
+        puts "  That means that unfortunately we can't sync the changes automatically"
         synched << store_issue
-      elsif github_changed
-        puts "On #{p.system_name} changing issue #{store_issue.pivotal_id} from '#{store_issue.title}' to '#{github_issue.title}'"
-        p.edit_issue(store_issue.pivotal_id, github_issue.title)
-        synched << Issue.new(github_issue.title, store_issue.github_id, store_issue.pivotal_id)
-      elsif pivotal_changed
-        puts "On #{g.system_name} changing issue #{store_issue.github_id} from '#{store_issue.title}' to '#{pivotal_issue.title}'"
-        g.edit_issue(store_issue.github_id, pivotal_issue.title)
-        synched << Issue.new(pivotal_issue.title, store_issue.github_id, store_issue.pivotal_id)
+      elsif issues_changed.select{|b| b}.count == 1
+        s.each do |t|
+          issue = t.find_issue_to_be_synched(store_issue.ids[t.tag])
+          if issue.title != store_issue.title
+            # Update the title on the other systems
+            (s - [t]).each do |repo|
+              puts "On #{repo.system_name} changing issue #{store_issue.ids[repo.tag]} from '#{store_issue.title}' to '#{issue.title}'"
+              repo.edit_issue(store_issue.ids[repo.tag], issue.title)
+              synched << SynchedIssue.new(issue.title, store_issue.ids)
+            end
+          end
+        end
       else
         synched << store_issue
       end
     end
-    github_issues.delete_if {|i| i.github_id == store_issue.github_id}
-    pivotal_issues.delete_if {|i| i.pivotal_id == store_issue.pivotal_id}
+    s.each do |t|
+      t.mark_as_synched(store_issue.ids[t.tag])
+    end
   end
 end
 
 # Any issues we see from here on we don't know anything about. i.e. we haven't seen them before and we haven't stored their
 # id's in the sync store
 
-matching_titles = github_issues.map{|i| i.title} & pivotal_issues.map{|i| i.title}
+all_titles = s.map do |t|
+  t.issues_to_be_synched.map{|i| i.title}
+end.flatten
+
+# We can do this because we can be sure that there is only one of a particular title in one repo because we checked for that earlier
+matching_titles = all_duplicates(all_titles)
 
 # We assume tickets with matching titles have been synced. So, we remove them from our list to process
 matching_titles.each do |t|
-  synched << Issue.new(t, github_issues.find{|i| i.title == t}.github_id, pivotal_issues.find{|i| i.title == t}.pivotal_id)
-  github_issues.delete_if {|i| i.title == t}
-  pivotal_issues.delete_if {|i| i.title == t}
+  ids = {}
+
+  s.each do |repo|
+    matching_issue = repo.issues_to_be_synched.find{|i| i.title == t}
+    if matching_issue
+      ids[repo.tag] = matching_issue.repo_id
+      repo.mark_as_synched(matching_issue.repo_id)
+    end
+  end
+
+  synched << SynchedIssue.new(t, ids)
 end
 
 # Now, remaining tickets in the github list need to be added to pivotal and vice versa
-puts "Adding new #{p.issues_name}..." unless github_issues.empty?
-github_issues.each do |i|
-  pivotal_id = p.new_issue(i.title)
-  synched << Issue.new(i.title, i.github_id, pivotal_id)
-end
-
-puts "Adding new #{g.issues_name}..." unless pivotal_issues.empty?
-pivotal_issues.each do |i|
-  github_id = g.new_issue(i.title)
-  synched << Issue.new(i.title, github_id, i.pivotal_id)
+s.each do |repo|
+  puts "Synching new issues added on #{repo.system_name}..." unless repo.issues_to_be_synched.empty?
+  repo.issues_to_be_synched.each do |i|
+    ids = {}
+    s.map do |t|
+      ids[t.tag] = (t == repo ? i.repo_id : t.new_issue(i.title))
+    end
+    synched << SynchedIssue.new(i.title, ids)
+  end
 end
 
 # Write out the issue sync store
